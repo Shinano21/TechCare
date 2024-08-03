1cecb0b2ac1e06ad629268cff2d23bf3c6736540
randiel20 <112055468+randiel20@users.noreply.github.com>
2024-08-03 13:38:37 +0800

    buhay ay di karera

--- admin/acreate.php
+++ admin/acreate.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ias2";
+$dbname = "techcare";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
