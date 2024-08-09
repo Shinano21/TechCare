635bcf85884af6b7de08fc953096a7db3a80359f
Jan-Jan20 <janjanarimado.alberto@bicol-u.edu.ph>
2024-08-09 12:33:28 +0800

    Add Resident.html:Added some modals/ fix birthday

--- admin/add_resident.php
+++ admin/add_resident.php
@@ -32,25 +32,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idNumber = $_POST['idNumber'];
     $momFirstName = $_POST['momFirstName'];
     $momLastName = $_POST['momLastName'];
-    $qrCode = $_POST['qrCode'];
+    $qrCode = $_POST['qrCode'] ?? null;
 
     $profilePic = null;
     if (!empty($_FILES['image']['tmp_name'])) {
         $profilePic = addslashes(file_get_contents($_FILES['image']['tmp_name']));
     }
 
-    $sql = "INSERT INTO residents (first_name, middle_name, last_name, suffix_name, gender, date_of_birth, place_of_birth, religion, citizenship, street, zone, barangay, municipal, province, zipcode, contact_number, education, occupation, civil_status, labor_status, voter_status, pwd_status, four_p, status, longitude, latitude, profile_pic, id_type, id_number, mom_name, mom_lname, qr_code)
-            VALUES ('$firstName', '$middleName', '$lastName', '$suffix', '$sex', '$dateOfBirth', '$placeOfBirth', '$religion', '$citizenship', '$street', '$zone', '$barangay', '$municipal', '$province', '$zipcode', '$contactNumber', '$education', '$occupation', '$civilStatus', '$laborStatus', '$voterStatus', '$pwdStatus', '$fourP', '$covidVaccineStatus', '$longitude', '$latitude', '$profilePic', '$idType', '$idNumber', '$momFirstName', '$momLastName', '$qrCode')";
+    $sql = "INSERT INTO residents 
+            (first_name, middle_name, last_name, suffix_name, gender, date_of_birth, place_of_birth, religion, citizenship, street, zone, barangay, municipal, province, zipcode, contact_number, education, occupation, civil_status, labor_status, voter_status, pwd_status, four_p, status, longitude, latitude, profile_pic, id_type, id_number, mom_name, mom_lname, qr_code)
+            VALUES 
+            ('$firstName', '$middleName', '$lastName', '$suffix', '$sex', '$dateOfBirth', '$placeOfBirth', '$religion', '$citizenship', '$street', '$zone', '$barangay', '$municipal', '$province', '$zipcode', '$contactNumber', '$education', '$occupation', '$civilStatus', '$laborStatus', '$voterStatus', '$pwdStatus', '$fourP', '$covidVaccineStatus', '$longitude', '$latitude', '$profilePic', '$idType', '$idNumber', '$momFirstName', '$momLastName', '$qrCode')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>
-                alert('New resident added successfully');
-                window.location.href = 'resident.php';
-              </script>";
+    if (mysqli_query($conn, $sql)) {
+        // Success response
+        $response = [
+            "success" => true,
+            "message" => "New resident added successfully!"
+        ];
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // Error response with SQL error
+        $response = [
+            "success" => false,
+            "message" => "Error: " . mysqli_error($conn)
+        ];
     }
 
+    // Close the database connection
     $conn->close();
+
+    // Return response in JSON format
+    echo json_encode($response);
 }
 ?>
